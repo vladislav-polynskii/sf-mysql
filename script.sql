754511c86ef8281b8bc7037cc352ffd918235bc6
root <root@sf-artifactory-ubuntu-2004.ru-central1.internal>
2023-10-30 07:29:13 +0000

    new comments

--- script.sql
+++ script.sql
@@ -1,7 +1,9 @@
+-- first command
 SELECT fr.rfam_acc, fr.rfamseq_acc, fr.seq_start, fr.seq_end
 FROM full_region fr, rfamseq rf, taxonomy tx
 LIMIT 1;
 
+-- second command
 SELECT fr.rfam_acc, fr.rfamseq_acc, fr.seq_start, fr.seq_end
 FROM full_region fr, rfamseq rf, taxonomy tx
 WHERE rf.ncbi_id = tx.ncbi_id
